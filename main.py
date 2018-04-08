a2c627a45ab81216f42ec75284b7e02b5e86802b
Denis Sukhoverkhov <at10tion.angry@yandex.ru>
2018-04-08 19:55:34 +0300

    created database.db

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-from flask import Flask
+import sqlite3
+
+from flask import Flask, g
 from flask.ext.sqlalchemy import SQLAlchemy
+
+DATABASE = 'database.db'
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
 
 
 def create_app(debug=False):
@@ -10,6 +21,12 @@ def create_app(debug=False):
 
 app = create_app()
 db = SQLAlchemy(app)
+
+
+@app.route('/')
+def index():
+    cur = get_db().cursor()
+
 
 if __name__ == '__main__':
     app.run()
